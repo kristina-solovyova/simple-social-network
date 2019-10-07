cb7602fe52fe914d2c179bbcbea2e41118f472be
Kristina Solovyova <kris.solo15@gmail.com>
2019-10-07 20:03:16 +0300

    Fix allowed hosts

--- social_net/settings.py
+++ social_net/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(',')
 
 
 # Application definition
